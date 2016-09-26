c1e05d1c1ea69236d0bdbe5009039b79306ab631
Zach Schultz <zacharymschultz@gmail.com>
2016-09-26 01:06:46 -0400

    Using nodemon in npm dev script for file watch and babel-node, fixed :id route for courses

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -3,6 +3,7 @@
 let axios = require('axios');
 let express = require('express');
 let router = express.Router();
+let { canvasApiUrl } = require('../../config/default');
 
 let canvasAuthMiddleWare = function(req, res, next) {
   // Expects Canvas Auth Token in req.headers.authorization
@@ -21,7 +22,7 @@ router.get('/', canvasAuthMiddleWare, function(req, res, next) {
   // @TODO Remove this to config as well, this is hardcoded for Fall 2016
   let currTerm = 1890;
   let courses = [];
-  axios.get(`https://ufl.instructure.com/api/v1/courses?access_token=${authToken}`)
+  axios.get(`${canvasApiUrl}access_token=${authToken}`)
     // Successful GET of courses
     // Iterate through each course, check if it is in the current term
     .then(function(response) {
@@ -48,22 +49,24 @@ router.get('/', canvasAuthMiddleWare, function(req, res, next) {
 /* GET /courses/id */
 router.get('/:id', canvasAuthMiddleWare, function(req, res, next) {
   let authToken = req.headers.authorization;
+  const courseId = parseInt(req.params.id);
   // let authToken = '1016~JzBQPzwPJzB58iOAADG5hP8hxdF2L20xSt70uE1KatDRwMiOxxyVwFXeHDwfD0nq';
 
   // @TODO Remove this to config as well, this is hardcoded for Fall 2016
   let currTerm = 1890;
-  let courses = [];
-  axios.get(`https://ufl.instructure.com/api/v1/courses?access_token=${authToken}`)
+  console.log(`looking for ${req.params.id}`);
+  axios.get(`${canvasApiUrl}access_token=${authToken}`)
     // Successful GET of courses
     // Iterate through each course, check if it is in the current term
     .then(function(response) {
-      response.data.forEach((course) => {
-        if (course.enrollment_term_id === currTerm)
-          courses.push(course);
+      let course = response.data.find((course) => {
+        return course.id === courseId;
       });
 
-      // Return the student's courses
-      res.send({'courses': courses});
+      if (course)
+        res.send({'course': course});
+      else
+        res.send({'statusText': `Course ${req.params.id} not found.`})
     })
     .catch(function(err) {
       // Handle an error
